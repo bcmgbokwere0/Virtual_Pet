4a8692757bd475f97fff5f234f69234093f4c1f7
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-07-12 01:19:24 -0500

    updated pet

--- src/main/VirtualPet.java
+++ src/main/VirtualPet.java
@@ -10,6 +10,10 @@ public class VirtualPet {
     // Constructor
 
     public VirtualPet() {
+        setHunger(100);
+        setThirst(100);
+        setTiredness(100);
+        setMood(100);
     }
 
     // Getter and Setters
@@ -56,8 +60,11 @@ public class VirtualPet {
     // Methods
     public void tick() {
         setHunger(getHunger() - 5);
+
         setThirst(getThirst() - 5);
+
         setTiredness(getTiredness() - 5);
+
         if (getHunger() >= 100 || getThirst() >= 100) {
             setMood(getMood() - 0);
         } else if (getHunger() >= 70 || getThirst() >= 70) {
@@ -70,34 +77,18 @@ public class VirtualPet {
     }
 
     public void feed() {
-        System.out.println("----------------------------------------------------------");
-        System.out.println("You gave " + getName() + " their favorite food! They seemed to really enjoy it...");
-        System.out.println("Hunger Level Increased by 20.");
-        System.out.println("----------------------------------------------------------");
         setHunger(getHunger() + 20);
     }
 
     public void drink() {
-        System.out.println("----------------------------------------------------------");
-        System.out.println("You gave " + getName() + " a glass of water. They really chugged it down...");
-        System.out.println("Thirst Level Increased by 20.");
-        System.out.println("----------------------------------------------------------");
         setThirst(getThirst() + 20);
     }
 
-    public void Play() {
-        System.out.println("----------------------------------------------------------");
-        System.out.println("You and " + getName() + " played a fun game of virtual volleyball. \033[3m You let them win...\033[0m");
-        System.out.println("Mood Increased by 30.");
-        System.out.println("----------------------------------------------------------");
+    public void play() {
         setMood(getMood() + 30);
     }
 
-    public void Sleep() {
-        System.out.println("----------------------------------------------------------");
-        System.out.println("You tucked " + getName() + " into their virtual bed, they really got comfortable...");
-        System.out.println(getName() + " became well rested.");
-        System.out.println("----------------------------------------------------------");
+    public void sleep() {
         setTiredness(100);
     }
 
--- src/main/VirtualPetApplication.java
+++ src/main/VirtualPetApplication.java
@@ -1,87 +1,155 @@
 package virtual_pet;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class VirtualPetApplication {
     public static void main(String[] args) {
-        List<VirtualPet> necessities = new ArrayList<VirtualPet>();
         Scanner gameplay = new Scanner(System.in);
+
         VirtualPet Pet = new VirtualPet();
-        Pet.setHunger(100);
-        Pet.setThirst(100);
-        Pet.setTiredness(100);
-        Pet.setMood(100);
-        necessities.add(Pet);
+
         int option;
         boolean quitGame = false;
+        boolean goodInput = false;
+
         System.out.println(
                 "Welcome to Brian's WCCI Virtual Pet Program! Here you can take care of your own virtual friend :)");
+
         System.out.println("Please input the name of your pet: ");
+
         Pet.setName(gameplay.nextLine());
+
         System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("Wow " + Pet.getName() + " is an awesome name.");
+
+        System.out.println("Wow " + Pet.getName() + " is an awesome name.\n");
+
+        System.out.println(
+                "Hope you're ready to have fun with your new virtual friend! You need to help take care of your pet "
+                        + Pet.getName()
+                        + " by feeding them, giving them water, playing with them, and making them happy! Lets get right into things!");
+
         System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("Hope you're ready to have fun with your new virtual friend! You need to help take care of your pet "+ Pet.getName() + " by feeding them, giving them water, playing with them, and making them happy! Lets get right into things!");
-        System.out.println("------------------------------------------------------------------------------------");
-                while (true) {
-            for (int i = 0; i < necessities.size(); i++) {
-                System.out.println(Pet.getName() + "'s Current Stats!");
-                System.out.println("Hunger: " + necessities.get(i).getHunger());
-                System.out.println("Thirst: " + necessities.get(i).getThirst());
-                System.out.println("Tiredness: " + necessities.get(i).getTiredness());
-                System.out.println("Mood: " + necessities.get(i).getMood());
-                System.out.println("----------------------------------------------------------");
-                System.out.println("What would you like to do with " + Pet.getName() + " today?");
-                System.out.println("1. Feed " + Pet.getName());
-                System.out.println("2. Water " + Pet.getName());
-                System.out.println("3. Play with " + Pet.getName());
-                System.out.println("4. Put " + Pet.getName() + " to sleep.");
-                System.out.println("5. Do Nothing");
-                System.out.println("6. End Game");
-            }
-            boolean goodInput = false;
-            for (;;) {
-            if (!gameplay.hasNextInt()) {
-                System.out.println("You must enter a number.");
-            } else {
-            option = gameplay.nextInt();
-            if (option <= 6 && option > 0){
-                goodInput = true;
-            }
-            if(goodInput){
-                break;
-            }
-            System.out.println("Your number must be selected from the options.");
-            }
-            gameplay.nextLine();
+
+        while (true) {
+            System.out.println(Pet.getName() + "'s Current Stats!");
+
+            System.out.println("Hunger: " + Pet.getHunger());
+
+            System.out.println("Thirst: " + Pet.getThirst());
+
+            System.out.println("Tiredness: " + Pet.getTiredness());
+
+            System.out.println("Mood: " + Pet.getMood());
+
+            System.out.println("----------------------------------------------------------");
+
+            System.out.println("What would you like to do with " + Pet.getName() + " today?");
+
+            System.out.println("1. Feed " + Pet.getName());
+
+            System.out.println("2. Water " + Pet.getName());
+
+            System.out.println("3. Play with " + Pet.getName());
+
+            System.out.println("4. Put " + Pet.getName() + " to sleep.");
+
+            System.out.println("5. Do Nothing");
+
+            System.out.println("6. End Game");
+
+            while (true) {
+                if (!gameplay.hasNextInt()) {
+
+                    System.out.println("You must enter a number.");
+                } else {
+                    option = gameplay.nextInt();
+
+                    if (option <= 6 && option > 0) {
+                        goodInput = true;
+                    }
+                    if (goodInput) {
+                        break;
+                    }
+
+                    System.out.println("Your number must be selected from the options.");
+
                 }
+
+                gameplay.nextLine();
+            }
             switch (option) {
+
                 case 1:
+                    System.out.println("----------------------------------------------------------");
+
+                    System.out.println(
+                            "You gave " + Pet.getName() + " their favorite food! They seemed to really enjoy it...");
+
+                    System.out.println("Hunger Level Increased by 20.");
+
+                    System.out.println("----------------------------------------------------------");
+
                     Pet.feed();
+
                     break;
+
                 case 2:
+                    System.out.println("----------------------------------------------------------");
+
+                    System.out
+                            .println("You gave " + Pet.getName() + " a glass of water. They really chugged it down...");
+
+                    System.out.println("Thirst Level Increased by 20.");
+
+                    System.out.println("----------------------------------------------------------");
+
                     Pet.drink();
                     break;
+
                 case 3:
-                    Pet.Play();
+                    System.out.println("----------------------------------------------------------");
+
+                    System.out.println("You and " + Pet.getName()
+                            + " played a fun game of virtual volleyball. \033[3m You let them win...\033[0m");
+
+                    System.out.println("Mood Increased by 30.");
+
+                    System.out.println("----------------------------------------------------------");
+
+                    Pet.play();
                     break;
+
                 case 4:
-                    Pet.Sleep();
+                    System.out.println("----------------------------------------------------------");
+
+                    System.out.println(
+                            "You tucked " + Pet.getName() + " into their virtual bed, they really got comfortable...");
+
+                    System.out.println(Pet.getName() + " became well rested.");
+
+                    System.out.println("----------------------------------------------------------");
+
+                    Pet.sleep();
                     break;
+
                 case 5:
                     break;
+
                 case 6:
                     gameplay.close();
                     quitGame = true;
                     break;
             }
+
             if (quitGame) {
                 break;
             }
+
             Pet.tick();
         }
+
         gameplay.close();
         System.out.println("Thanks for Playing, " + Pet.getName() + " will miss you :).");
     }
+
 }
